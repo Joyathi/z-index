f7c9a9af7df89bb16824c10e4011bcbd929d28f7
athin <athinjoy14chempananickal@gmail.com>
2024-01-31 09:23:24 +0530

    new

--- javascript/script7.js
+++ javascript/script7.js
@@ -1,15 +1,83 @@
-//25 -01-2024
+// let detailes={
+//     firstName :"jack",
+//     lastName : "rose",
+//     email : "jack@gmail.com",
+// }
+//  let fruits = ["apple","orange","mango"];
 
-function countDwon(n){
+//  //Destructuring 
+//  let {firstName,lastName,email} = details;
+//  console.log ("firstName :",firstname);
+//  console.log ("lastName :",lasttname);
+//  console.log ("email :",email);
 
-    if (n==0){
-        console.log("Done");
-    }else {
-        console.log(n);
-        countDwon(n-1);//recursive call (A function calling it self)
+//  let [fruit1,fruit2,fruit3] = fruits;
+//  console.log ("fruit1 ;",fruit1);
+//  console.log ("fruit2 ;",fruit2);
+//  console.log ("fruit3 ;",fruit3);
+
+ let details={
+    firstName :"jack",
+    lastName : "rose",
+    email : "jack@gmail.com",
+    address : {
+        street : "mystreet",
+        city : "mycity",
+        houseNo : 1,
     }
 }
-countDwon(3);//Normal function call
+ let fruits = ["apple","orange","mango",["avocado","grapes","pineapple"]];
 
-//n=3 -> else block -> logs 3 -> recursive call countDown(2)
-            
+ //Destructuring 
+ let {firstName,lastName,email ,address: {street,city,houseNo}} = details;
+ console.log ("firstName :", firstName);
+ console.log ("lastName :", lastName);
+ console.log ("email :",email);
+ console.log ("street :" ,street);
+ console.log ("city :",city);
+ console.log ("houseNo :",houseNo);
+
+ let [fruit1,fruit2,fruit3] = fruits;
+ console.log ("fruit1 ;",fruit1);
+ console.log ("fruit2 ;",fruit2);
+ console.log ("fruit3 ;",fruit3);
+
+ 
+ 
+ //Array function
+ let cars = ["Benz","BMW","Maruthi","Audi","BMW"];
+
+ //forEach
+ cars.forEach((element)=>{
+    console.log(element);
+ })
+
+ //Find
+ let findResult = cars.find((element)=>{
+    return element === "BMW";
+ })
+
+ console.log("findResult : ", findResult);
+
+ //Filter
+ let filterResult = cars.filter((element)=>{
+    return element === "BMW";
+ });
+
+ console.log ("filterResult : ",filterResult);
+
+ //Map
+ let mapResult = cars.map((element) =>{
+    return element = "my-" + element;
+    //return element ==="BMW";
+ });
+
+ console.log("mapResult :",mapResult);
+
+ //Reduce
+ let nums = [1,2,3,4,5];
+ let reduceResult = nums.reduce ((sum,element)=>{
+
+ },0);
+
+ console.log("reduceResult :",reduceResult);
