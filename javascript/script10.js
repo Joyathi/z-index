9d44bfa0dc573c147b9f1ebd2c574756c3debfdc
athin <athinjoy14chempananickal@gmail.com>
2024-02-16 09:13:27 +0530

    new

--- javascript/script10.js
+++ javascript/script10.js
@@ -76,6 +76,84 @@ const result16= regexp16.test (str1);//Here it matches fishe, fishes,fishesssss,
 console.log ("result16 : ",result1);//ture
 
 
+const str5 = "Hello";
+
+const regexp17 =/[kjm]/i;
+const result17= regexp17.test(str5); // if there is a 'k' or 'j' or 'm' then it will be ture
+console.log ("result17 : ",result17);//false
+
+const regexp18 =/[^kjm]/i
+const result18= regexp18.test (str5);//if there is no 'k' or 'j' or 'm' then it will be ture
+console.log ("result18 : ",result18);//ture
+
+const regexp19 =/[H+]/i;
+const result19= regexp19.test (str5);// at least one 'H' is required
+console.log ("result19 : ",result19);//ture
+
+const regexp20 =/[o$]/i;
+const result20= regexp20.test (str5);// string ends with o
+console.log ("result20 : ",result20);//ture
+
+const regexp21 =/o?$/i;
+const result21= regexp19.test (str5);// 'o' in string ending is optional\
+console.log ("result21 : ",result21);//ture
+
+//{} - indicates the number of characters, a{2} means "aa", ie 2 a is required
+const regexp23 =/L{2}o$/i;//'o' should exactly after 2 L's
+const result23= regexp23.test (str5);
+console.log ("result23 : ",result23);//ture
+
+const regexp24 =/L{2,4}o$/i;//'o' should exactly after between 2 to 4 L's
+const result24= regexp24.test (str5);
+console.log ("result24 : ",result24);//ture
+
+const regexp25 =/[H+]/i;//'o' should exactly after 2 to 4 L's and 'o' is required
+const result25= regexp25.test (str5);
+console.log ("result25 : ",result25);//ture
+
+const regexp26 =/HeL{2,}o$/i;//2 or more 'L' is required
+const result26= regexp26.test (str5);
+console.log ("result26 : ",result26);//ture
+
+// \ - indicates escape charecter, used for matching any characters which has special meaning
+const regexp27 =/\d/i;//if there is any digits, it will be ture , else it will be false
+const result27= regexp27.test (str5);
+console.log ("result27 : ",result27);//false
+
+const regexp28 =/\D/i;//if there is any digits, it will be ture, if there is only digits it will be false
+const result28= regexp28.test (str5);
+console.log ("result28 : ",result28);//ture
+
+//Suppose we want to match '.' in a string, since '.' has special meaning in regular experssions ot will not works as expected and will matching everything
+
+const regexp29 =/./i;//if there is a '.' or anything it will match
+const result29= regexp29.test (str5);
+console.log ("result29 : ",result29);//ture
+
+//so we can write it as
+
+const regexp30 =/\./i;//if there is any '.' in the string it match, else it will be false
+const result30= regexp30.test (str5);
+console.log ("result30 : ",result30);//false
+
+//so '\' (backslash) is used to escape special charactors
+
+//Date validation
+
+//1
+const regexpForDate =/^\d{1,2}-\d{1,2}-\d{4$}/i;//string should starts with 1 or 2 digits, then after '-' symbol, then again 1 or 2  digits, after '-', ending  will be 4
+
+//2
+
+// |- indicates or case 0|1 means either 0 or 2 can occur
+
+
+
+
+
+
+
+
 
 
 
