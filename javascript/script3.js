0165599a6114040057d0123f2d6516fd7017dcfa
athin <athinjoy14chempananickal@gmail.com>
2024-02-20 11:39:38 +0530

    new

--- javascript/script3.js
+++ javascript/script3.js
@@ -33,3 +33,68 @@ console.log("maxValue : ", maxValue)
 {
     console.log("add")
 }
+
+
+
+function countDown(n){
+        
+    if (n==0){
+        console.log("Done");
+    }else{
+        console.log(n);
+        countDown(n-1)//recursive call(a function calling itself)
+    }
+}
+
+countDown(3); // Normal function call
+
+// n=3 ->else block -> logs 3 -> recursive call countDwon(2)
+
+             //n=2 -> else block -> logs2 ->recursive call countDown(1)
+
+             //n=1 -> else block -> logs 1 ->recurive call countDwon(0)
+                //n=0 -> if block ->logs Done
+
+                
+//work 
+//factoral of a number using reccursive method
+//do some research about DOM
+
+//DOM methods
+
+let headings = document.getElementsByName('h1');
+console.log('headings[0] :',heading[0].textcontent);
+console.log('headings[0] :',heading[0]);
+
+let containers
+console.log("divElement1 :",divElement1.textcontent);\
+
+
+let button = Document.getElementByid('id');
+console.log("button :",button);
+
+button.addEventListener('mouseover',function(){
+    //alert("Button clicked"):
+    console.log("click event fired...")
+})
+
+button.addEventListener('mouseout',function(){
+    console.log("click event fired...")
+})
+
+button.addEventListener('mousedown',function(){
+    console.log("click event fired...")
+})
+
+
+button.addEventListener('mouseup',function(){
+    console.log("click event fired...")
+})
+
+button.addEventListener('mousemove',function(){
+    console.log("click event fired...")
+})
+
+button.addEventListener('keydown',function(){
+    console.log("click event fired...")
+})
