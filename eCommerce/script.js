1be0c8ff63b530ba098176cacfe3af0d5707a822
athin <athinjoy14chempananickal@gmail.com>
2024-02-23 06:50:00 +0530

    add

--- eCommerce/script.js
+++ eCommerce/script.js
@@ -1,38 +1,44 @@
 let xhr = new XMLHttpRequest();
-xhr.open("get",'https://fakestoreapi.com/products')
 
+xhr.open("get", 'https://fakestoreapi.com/products');
 xhr.send();
 
-xhr.onreadystatechange = function (){
-    console.log("readystate : ", xhr.readyState);
-    console.log("status :",xhr.status);
+xhr.onreadystatechange = function () {
+    console.log('Ready State :',xhr.readyState);
+    console.log("Status :",xhr.status);
 
-    if (xhr.readyState===4){
-        if (xhr.status===200);{
+    if (xhr.readyState === 4) {
+        if (xhr.status === 200); {
+            let result = xhr.response;
             console.log("result :",xhr.response);
-            console.log("type of (result) :",type(result));
+            console.log("type of(result) :",typeof(result));
 
             let parsed_result = JSON.parse(result);
-            console.log("parsed_result :",parsed_result);
-            //console.log('typeof(parsedresult):',typeof(parsed_result))
+            console.log("parsed_result : ",parsed_result);
+            // console.log('typeof(parsedresult) :',typeof(parsed_result))
 
-            let box = document.getElementById("container");
+            let box = document.getElementById('container');
             console.log("box :",box)
 
-            let box1 = "";
+            let box1 = " ";
 
-             for(let i =0; i<parsed_result.lenth; i++){
-                 box1= box1 +
-                 <div class="box">
-                     <p class="id">${parsed_result[i].id}</p>
-                     <h3 class="title">${parsed_result[i].title}</h3>
-                     <p class="price">${parsed_result[i].price}</p>
-                     <p class="description">${parsed_result[i].description}</p>
-                     <p class="id">${parsed_result[i].category}</p>
-                  
-                 </div>
+            for (let i = 0; i < parsed_result.length; i++) {
+                box1 = box1 + `
+                <div class="box">
+                    <p class="id">${parsed_result[i].id}</p>
+                    <h3 class="title">${parsed_result[i].title}</h3>
+                    <p class="price">${parsed_result[i].price} $</p>
+                    <p class="description">${parsed_result[i].description}</p>
+                    <p class="category">${parsed_result[i].category}</p>
+                    <img src=${parsed_result[i].image} alt="product_image" class = "pic" >
+                
+                    <p class="rate">${parsed_result[i].rate}</p>
+                    <p class="count">${parsed_result[i].count}</p>
+                </div>`
             }
-            box.innerHTML =box1;
-        }
-    }
+
+            box.innerHTML = box1;
+        }
+
+    }
 }
