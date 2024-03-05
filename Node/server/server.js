91b1c83d15ea5301d9d3e348949871c7cb17473d
athin <athinjoy14chempananickal@gmail.com>
2024-03-05 09:21:23 +0530

    add

--- Node/server/server.js
+++ Node/server/server.js
@@ -2,7 +2,26 @@ const http = require('http');
 const url = require('url');
 const port = 3000;
 const fs = require('fs');
+const querystring = require('querystring');
+const { MongoClient } =require('mongodb');
+const { error } = require('console');
 
+//connection URL
+const mongodb_url = "mongodb://localhost:27017"
+const client = new MongoClient(mongodb_url);
+
+//Database Name
+const dbName = 'users';
+const collName = "users_coll";
+
+async function connect() {
+    //use connect method to connect to the server
+    await client.connect();
+
+    console.log ('connected successfully to server');
+
+    return 'done.';
+}
 
 const server = http.createServer((req,res) =>{
 
@@ -17,7 +36,7 @@ const server = http.createServer((req,res) =>{
         res.writeHead(200,{'content-Type' : 'text/html'});
         res.end(fs.readFileSync('../client/index.html'));
 
-    } else if(parsed_url.pathname === '/style.css' && req.methord === 'GET'){
+    } else if(parsed_url.pathname === "/style.css" && req.methord === 'GET'){
         res.writeHead(200,{'Content-Type' : "text/css"});
         res.end(fs.readFileSync('../client/style.css'));
 
@@ -61,7 +80,17 @@ const server = http.createServer((req,res) =>{
     }
 
 });
-server.listen(port,() =>{
-    console.log(`Server running at http://localhost:${port}`);
-})
 
+
+connect()
+  .then((message) =>{
+      console.log("message :",message);
+  })
+  .catch ((error) =>{
+    console.log("error :",error);
+  })
+  .finally(() =>{
+    server.listen(port,() =>{
+        console.log(`Server running at http://localhost:${port}`);
+    })
+  })
