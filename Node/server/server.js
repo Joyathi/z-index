6bf3a7094471a401679d7e47edbcba025a27fd1a
athin <athinjoy14chempananickal@gmail.com>
2024-03-06 09:27:53 +0530

    add

--- Node/server/server.js
+++ Node/server/server.js
@@ -3,7 +3,7 @@ const url = require('url');
 const port = 3000;
 const fs = require('fs');
 const querystring = require('querystring');
-const { MongoClient } =require('mongodb');
+const { MongoClient } = require('mongodb');
 const { error } = require('console');
 
 //connection URL
@@ -18,64 +18,93 @@ async function connect() {
     //use connect method to connect to the server
     await client.connect();
 
-    console.log ('connected successfully to server');
+    console.log('connected successfully to server');
 
     return 'done.';
 }
 
-const server = http.createServer((req,res) =>{
+
+
+const server = http.createServer((req, res) => {
+
+    const db = client.db(dbName);
+    const collection = db.collection(collName);
 
     let req_url = req.url;
-    console.log("req_url :",req_url);
+    console.log("req_url :", req_url);
 
-    let parsed_url =url.parse(req_url);
-    console.log("parsed_url :",parsed_url);
+    let parsed_url = url.parse(req_url);
+    console.log("parsed_url :", parsed_url);
 
-    if(parsed_url.pathname === '/' && req.method === "GET"){
+    if (parsed_url.pathname === '/' && req.method === "GET") {
 
-        res.writeHead(200,{'content-Type' : 'text/html'});
+        res.writeHead(200, { 'content-Type': 'text/html' });
         res.end(fs.readFileSync('../client/index.html'));
 
-    } else if(parsed_url.pathname === "/style.css" && req.methord === 'GET'){
-        res.writeHead(200,{'Content-Type' : "text/css"});
+    } else if (parsed_url.pathname === "/style.css" && req.methord === 'GET') {
+        res.writeHead(200, { 'Content-Type': "text/css" });
         res.end(fs.readFileSync('../client/style.css'));
 
-    }  else if(parsed_url.pathname === '/script.js' && req.methord === 'GET'){
-        res.writeHead(200,{'Content-Type' : "text/javascript"});
+    } else if (parsed_url.pathname === '/script.js' && req.methord === 'GET') {
+        res.writeHead(200, { 'Content-Type': "text/javascript" });
         res.end(fs.readFileSync('../client/script.js'));
     }
 
-    if(req.methord === "POST" && parsed_url.pathname === "/submit"){
+    if (req.methord === "POST" && parsed_url.pathname === "/submit") {
 
-        let body="";
+        let body = "";
 
-        req.on('data',(chunks) => {
-            console.log("Chunks :",chunks);
+        req.on('data', (chunks) => {
+            console.log("Chunks :", chunks);
             body = chunks.toString();
-            console.log("body :",body);
+            console.log("body :", body);
         })
 
-        req.on("end",() =>{
+        req.on("end", async () => {
             let formDatas = querystring.parse(body);
-            console.log("formDatas :",formDatas);
+            console.log("formDatas :", formDatas);
 
             //Do something with the submitted data (eg : save to a database)
-            console.log("Name :",formDatas.name);
-            console.log("email :",formDatas.email);
-            console.log("password :",formDatas.password);
+            console.log("Name :", formDatas.name);
+            console.log("email :", formDatas.email);
+            console.log("password :", formDatas.password);
 
-            let response ={
-                success :true,
-                statusCode :200,
-                data : formDatas,
-                message : "Form Datas submitted successfully",
-            }
+            //inserting formDatas to the database
+            await collection.insertOne(formDatas)
+                .then((message) => {
+                    console.log("Document inserted successfully");
+
+                    let response = {
+                        success: true,
+                        statusCode: 200,
+                        data: formDatas,
+                        message: "FormDatas submitted successfully"
+
+                    }
+              
 
             let json_response = JSON.stringify(response);
-            console.log("json_response :",json_response)
+            console.log("json_response :", json_response)
 
-            res.writeHead(200,{"content-Type" :"text/json"})
+            res.writeHead(200, { "content-Type": "text/json" })
             res.end(json_response)
+        })
+            .catch((error) => {
+                console.log("Document insertion failed");
+
+                let response = {
+                    success: false,
+                    statusCode: 400,
+                    data: formDatas,
+                    message: "Failed"
+                }
+                let json_response = JSON.stringify(response);
+                console.log("json_response :", json_response)
+
+                res.writeHead(response.statusCode, { "content-Type": "text/json" })
+                res.end(json_response)
+            })
+
         })
     }
 
@@ -83,14 +112,14 @@ const server = http.createServer((req,res) =>{
 
 
 connect()
-  .then((message) =>{
-      console.log("message :",message);
-  })
-  .catch ((error) =>{
-    console.log("error :",error);
-  })
-  .finally(() =>{
-    server.listen(port,() =>{
-        console.log(`Server running at http://localhost:${port}`);
+    .then((message) => {
+        console.log("message :", message);
     })
-  })
+    .catch((error) => {
+        console.log("error :", error);
+    })
+    .finally(() => {
+        server.listen(port, () => {
+            console.log(`Server running at http://localhost:${port}`);
+        })
+    })
